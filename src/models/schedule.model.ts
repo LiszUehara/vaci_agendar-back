95166ff0c10c951bbec2be7b559fdcdb6bd69070
LiszUehara <ueharalisandra@gmail.com>
2024-07-07 18:19:39 -0300

    :recycle: refactor: removing unused interface

--- src/models/schedule.model.ts
+++ src/models/schedule.model.ts
@@ -1,14 +1,6 @@
 import zod from "zod";
 import { IPatient } from "./patient.model";
 
-export interface ISchedule {
-    id: string;
-    time: Date;
-    createAt?: Date;
-    updatedAt?: Date;
-    patients: IPatient[] 
-}
-
 export let scheduleSchema = zod.object({
     dateTime: zod.coerce.date({message: "Campo dateTime esta invalido"}),
     status: zod.enum(["completed", "cancelled", "unrealized", "other"]).nullable(),
